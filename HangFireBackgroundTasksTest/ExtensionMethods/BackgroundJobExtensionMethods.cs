3fb817cdce16dd0a70c6c4fdeb6c15a402ca7898
leechuckfon <43467037+leechuckfon@users.noreply.github.com>
2019-11-14 08:46:44 +0100

    Update BackgroundJobExtensionMethods.cs

--- HangFireBackgroundTasksTest/ExtensionMethods/BackgroundJobExtensionMethods.cs
+++ HangFireBackgroundTasksTest/ExtensionMethods/BackgroundJobExtensionMethods.cs
@@ -4,12 +4,7 @@ using System;
 
 namespace HangFireBackgroundTasksTest {
     public static class BackgroundJobExtensionMethods {
-        public static string ConfigString  = @"{'DestinationPath' : '\\10.3.32.127\Invoices\Medius',
-                'MediusBaseUrl' : 'https://cloud.mediusflow.com/kinepolisQA',
-                'Scope' : 'Integration',
-                'GrantType' : 'client_credentials',
-                'Client_id' : '42aa24eb61d94114412b1d6328e961fa',
-                'Client_secret' : 'Ng-gVgD2nDULBjKIW4NbarxtkJcteQMjCzXy4VfCoi8'}";
+       
         public static void QueueBackgroundJobs(this IBackgroundJobClient backgroundJobs) {
 
             backgroundJobs.Enqueue(() => Job2.Run());
